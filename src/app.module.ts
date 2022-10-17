d0211ecea5b9940c34c8c279b65ffc3c76c76df2
Igor Murashev <igmur00@mail.ru>
2022-10-17 12:33:20 +0300

    mailer

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,12 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-import { ContactsModule } from './contacts/contacts.module';
-import { FeedbackModule } from './feedback/feedback.module';
-import { PracticesModule } from './practices/practices.module';
-import { AuthModule } from './auth/auth.module';
 import { MailModule } from './mail/mail.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ envFilePath: `.${process.env.NODE_ENV}.env` }),
-    MongooseModule.forRoot('mongodb://mongo/27017'),
-    AuthModule,
-    ContactsModule,
-    FeedbackModule,
-    PracticesModule,
-    MailModule,
-  ],
+  imports: [MailModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,30 +1,29 @@
 import { MailerService } from '@nestjs-modules/mailer';
-import {HttpException, HttpStatus, Injectable} from '@nestjs/common';
-import { CreateFeedbackDto } from "../feedback/dto/create-feedback.dto";
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { CreateFeedbackDto } from '../feedback/dto/create-feedback.dto';
 
 @Injectable()
 export class MailService {
-    constructor(private mailerService: MailerService) {}
+  constructor(private mailerService: MailerService) {}
 
-    async sendFeedbackMail(feedback: CreateFeedbackDto): Promise<void> {
-        try {
-            await this.mailerService.sendMail({
-                to: `${process.env.USER_TO_EMAIL}`,
-                subject: 'Обратная связь от пользователя с сайта bbp.ru',
-                template: './feedbackMail',
-                context: {
-                    name: feedback.name,
-                    phone: feedback.phone,
-                    question: feedback.question,
-                    email: feedback.email
-                },
-            });
-        } catch (error) {
-            throw new HttpException(
-                `Ошибка работы почты: ${JSON.stringify(error)}`,
-                HttpStatus.UNPROCESSABLE_ENTITY,
-            );
-        }
-
+  async sendFeedbackMail(feedback: CreateFeedbackDto): Promise<void> {
+    try {
+      await this.mailerService.sendMail({
+        to: `${process.env.USER_TO_EMAIL}`,
+        subject: 'Обратная связь от пользователя с сайта bbp.ru',
+        template: './feedbackMail',
+        context: {
+          name: feedback.name,
+          phone: feedback.phone,
+          question: feedback.question,
+          email: feedback.email,
+        },
+      });
+    } catch (error) {
+      throw new HttpException(
+        `Ошибка работы почты: ${JSON.stringify(error)}`,
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
     }
+  }
 }
